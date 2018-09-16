826b94705a0a1bf97df63c528b3901cf23e5ea3a
Islam Elsayed <islam.elsayed.mohammed@gmail.com>
2018-09-16 15:42:05 +0200

    add index/show actions for movies controller

--- app/controllers/v1/movies_controller.rb
+++ app/controllers/v1/movies_controller.rb
@@ -1,3 +1,11 @@
 class MoviesController < ApplicationController
+  def index
+    result = Movies::SearchService.new(params[:keyword]).execute
+    render json: result
+  end
 
+  def show
+    movie = Movies::ShowService.new(params[:movie_id]).execute
+    render json: movie
+  end
 end
