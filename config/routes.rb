40567b59e1c11c3680eb288687e2fe16efb1d2cf
Islam Elsayed <islam.elsayed.mohammed@gmail.com>
2018-09-18 15:24:20 +0200

    add routes form movies/favourites resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  namespace :v1 do
-    devise_for :users, controllers: { registrations: "v1/users/registrations", sessions: "v1/users/sessions", passwords: "devise/passwords" }
+  devise_for :users, path: 'v1/users', controllers: { registrations: "v1/users/registrations", sessions: "v1/users/sessions" }
+  namespace :v1, defaults: { format: 'json' } do
+    resources :movies, only: [:index, :show]
+    resources :favorites, param: :mdb_id,only: [:create, :destroy, :index]
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
