4d0164fa17f808080e3367454e2d4c28d6686d8e
Islam Elsayed <islam.elsayed.mohammed@gmail.com>
2018-09-18 15:23:33 +0200

    add favorites associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,8 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable, :recoverable,
-         :rememberable, :validatable, :jwt_authenticatable,jwt_revocation_strategy: self
+         :rememberable, :validatable, :jwt_authenticatable, jwt_revocation_strategy: self
+
+  # associations
+  has_many :favorites
 end
