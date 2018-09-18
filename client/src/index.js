0e098e7b62df2a9ce27a5209e60cb0ddb63501fb
Islam Elsayed <islam.elsayed.mohammed@gmail.com>
2018-09-18 15:26:17 +0200

    add frontend routes

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import Movies from './movies/Movies';
+import Movie from './movies/Movie';
+import Login from './users/Login';
+import Register from './users/Register';
+// Import routing components
+import { BrowserRouter, Link, Route } from 'react-router-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+                <BrowserRouter>
+                  <div>
+                    <Route exact path="/" component={Movies}/>
+                    <Route exact path="/movies/:id" component={Movie}/>
+                    <Route exact path="/login" component={Login}/>
+                    <Route exact path="/register" component={Register}/>
+                  </div>
+               </BrowserRouter>,
+               document.getElementById('root'));
 registerServiceWorker();
